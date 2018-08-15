5fb802319f0c66a602bcd1bbdaa365a122b7055b
Sanne <sanne.van.mourik@capgemini.com>
2018-08-15 17:48:52 +0200

    create getter and setter unittests for the class SubDish

--- src/test/java/com/MolvenoLakeResort/model/restaurant/SubDishGetterAndSetterTest.java
+++ src/test/java/com/MolvenoLakeResort/model/restaurant/SubDishGetterAndSetterTest.java
@@ -8,7 +8,6 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
