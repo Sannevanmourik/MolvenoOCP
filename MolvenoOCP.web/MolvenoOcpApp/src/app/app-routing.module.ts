09529d4a2f7370bec6a7c1b53e3350120dfdb09f
Eva van Rooden <evavanrooden@gmail.com>
2018-08-21 13:33:24 +0200

    added routerLinks to ingredient components

--- MolvenoOCP.web/MolvenoOcpApp/src/app/app-routing.module.ts
+++ MolvenoOCP.web/MolvenoOcpApp/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {IngredientListComponent} from '../app/components/ingredient-list/ingredient-list.component';
+import {IngredientEditorComponent} from '../app/components/ingredient-editor/ingredient-editor.component';
 
 const routes: Routes = [
-  { path: 'ingredients', component: IngredientListComponent }
+  { path: 'ingredients', component: IngredientListComponent },
+  { path: 'ingredientEditor', component: IngredientEditorComponent },
+
 ];
 
 
