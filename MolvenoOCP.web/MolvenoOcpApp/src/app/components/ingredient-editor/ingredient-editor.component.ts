de7b48916ff5a8c494df325f975eaad16c9cfa66
Eva van Rooden <evavanrooden@gmail.com>
2018-08-21 15:26:23 +0200

    fixed add ingredient button

--- MolvenoOCP.web/MolvenoOcpApp/src/app/components/ingredient-editor/ingredient-editor.component.ts
+++ MolvenoOCP.web/MolvenoOcpApp/src/app/components/ingredient-editor/ingredient-editor.component.ts
@@ -14,44 +14,57 @@ export class IngredientEditorComponent {
 
   ingredientForm = this.fb.group({
     name: ['', Validators.required],
-    price: ['', Validators.required],
-    vegetarian: [''],
-    stock: [''],
-    allergy: [''],
-    aliases: this.fb.array([
-      this.fb.control('')
-    ])
+    price: [0, Validators.required],
+    vegetarian: [false],
+    stock: [0],
+    allergy: [null],
+    // ingredients: this.fb.array([
+    //   this.fb.control('')
+    // ])
   });
 
-  get aliases() {
-    return this.ingredientForm.get('aliases') as FormArray;
-  }
+  // get aliases() {
+  //   return this.ingredientForm.get('aliases') as FormArray;
+  // }
 
   constructor(private fb: FormBuilder, private ingredientService: IngredientService) { }
 
 
-  addAlias() {
-    this.aliases.push(this.fb.control(''));
-  }
+  // addAlias() {
+  //   this.aliases.push(this.fb.control(''));
+  // }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.ingredientForm.value);
   }
 
-  add(name: string, price: number, vegetarian: boolean, stock: number, allergy: string): void {
-    // this.editMovie = undefined;
-    // name = name.trim();
-    // allergy = allergy.trim();
-    if (!name) { return; }
+  // add(name: string, price: number, vegetarian: boolean, stock: number, allergy: string): void {
+  //   // this.editMovie = undefined;
+  //   // name = name.trim();
+  //   // allergy = allergy.trim();
+  //   if (!name) { return; }
 
-    const newIngredient: Ingredient = { name, price, vegetarian, stock, allergy } as Ingredient;
-    this.ingredientService.addIngredient(newIngredient)
-      .subscribe(ingredient => {
-        this.ingredients.push(ingredient);
-        console.log('Ingredients now contains', this.ingredients);
-        this.ingredientService.getAll();
-      });
+  //   const newIngredient: Ingredient = { name, price, vegetarian, stock, allergy } as Ingredient;
+  //   this.ingredientService.addIngredient(newIngredient)
+  //     .subscribe(ingredient => {
+  //       this.ingredients.push(ingredient);
+  //       console.log('Ingredients now contains', this.ingredients);
+  //       this.ingredientService.getAll();
+  //     });
+  // }
+
+  add2() {
+    const formValue = this.ingredientForm.value;
+    const newIngredient = new Ingredient();
+    newIngredient.name = formValue.name;
+    newIngredient.price = formValue.price;
+    newIngredient.vegetarian = formValue.vegetarian;
+    newIngredient.stock = formValue.stock;
+    newIngredient.allergy = formValue.allergy;
+    console.log(newIngredient);
+
+    this.ingredientService.addIngredient(newIngredient).subscribe();
   }
 
 }
