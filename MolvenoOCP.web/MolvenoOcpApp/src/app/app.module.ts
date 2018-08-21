3ca3a48d134140ff5e8b69f6b6c0ff8e7f37008a
hbosch <hboschcap@gmail.com>
2018-08-21 11:17:08 +0200

    setting-up ngbootstrap

--- MolvenoOCP.web/MolvenoOcpApp/src/app/app.module.ts
+++ MolvenoOCP.web/MolvenoOcpApp/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { IngredientService } from '../app/services/ingredient-service.service';
 import { IngredientListComponent } from '../app/components/ingredient-list/ingredient-list.component';
 import { AppComponent } from './app.component';
 import { PlaceOrderComponent } from './components/place-order/place-order.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IngredientListComponent,
     PlaceOrderComponent,
+    NavComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
