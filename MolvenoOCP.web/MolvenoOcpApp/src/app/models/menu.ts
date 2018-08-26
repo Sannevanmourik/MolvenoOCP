6572408492eb6cac35478f2d3a51683fdb721d14
hbosch <hboschcap@gmail.com>
2018-08-26 17:42:08 +0200

    start implementing reactive form

--- MolvenoOCP.web/MolvenoOcpApp/src/app/models/menu.ts
+++ MolvenoOCP.web/MolvenoOcpApp/src/app/models/menu.ts
@@ -1,17 +1,73 @@
-import { Seasons } from './seasons';
+import { Ingredient } from '../models/ingredient';
+import { Allergy } from '../models/allergy';
+import { Menucategory } from '../models/menucategory.enum';
+import { Subdish } from '../models/subdish';
+import { Seasons } from '../models/seasons';
 
-export class Menu {
+export interface Menu {
     id: number;
     name: string;
     season: Seasons;
     salesPrice: number;
-    menuCategory: string[];
+    menuCategory: Menucategory;
     amountOfTimesOrdered: number;
-    ingredientList: string[];
-    subDishList: string[];
-    profit: number;
-    filteredListOfAllergiesPerMenuItem: string[];
+    ingredientList: IngredientsInterface[];
+    subDishList: SubdishesInterface[];
+    profit; number;
+    filteredListOfAllergiesPerMenuItem: ListOfAllergiesPMI[];
     calculatedPrice: number;
     vegetarian: boolean;
-    ingredientsInStock: number;
+    ingredientsInStock: boolean;
 }
+
+    export interface IngredientsInterface {
+        allergy: string;
+        id: number;
+        inStock: boolean;
+        name: string;
+        price: number;
+        stock: number;
+        vegetarian: boolean;
+    }
+
+    export interface SubdishesInterface {
+        id: number;
+        ingredientListForSubDish: SubdishesIngredientInterface[];
+        name: string;
+        vegetarian: boolean;
+    }
+
+        export interface SubdishesIngredientInterface {
+            allergy: string;
+            id: number;
+            inStock: boolean;
+            name: string;
+            price: number;
+            stock: number;
+            vegetarian: boolean;
+        }
+
+
+    export interface ListOfAllergiesPMI {
+        id: number;
+    }
+
+
+
+// import { Seasons } from './seasons';
+
+// export class Menu {
+//     id: number;
+//     name: string;
+//     season: Seasons;
+//     salesPrice: number;
+//     menuCategory: string[];
+//     amountOfTimesOrdered: number;
+//     ingredientList: string[];
+//     subDishList: string[];
+//     profit: number;
+//     filteredListOfAllergiesPerMenuItem: string[];
+//     calculatedPrice: number;
+//     vegetarian: boolean;
+//     ingredientsInStock: number;
+// }
